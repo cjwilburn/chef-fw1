d025d60527a73c5af8513f65f96d026316faf011
Courtney Wilburn <wilburnc@wharton.upenn.edu>
2012-10-15 15:37:27 -0400

    stuff for new git configuration. not needed

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['fw1']['install_path'] = "/vagrant/wwwroot/frameworks" 
+default['fw1']['install_path'] = "/vagrant/frameworks" 
 default['fw1']['git']['repository'] = "https://github.com/seancorfield/fw1.git" 
 default['fw1']['git']['revision'] = "master"
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "wilburnc@wharton.upenn.edu"
 license          "Apache 2.0"
 description      "Installs ColdFusion Framework One"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 supports 'ubuntu', '= 10.04'
 supports 'ubuntu', '>= 11.04'
@@ -11,3 +11,4 @@ supports 'ubuntu', '>= 11.04'
 
 
 recipe "default", "default recipe"
+recipe "fw1", "pulls repo from github and places it in top-level frameworks directory"
